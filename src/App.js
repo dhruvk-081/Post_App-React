4ddc3e60303ab2ae894bae75972c5fee1ad5e8bf
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-02-19 04:44:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import RenderApi from "./components/RenderApi";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <RenderApi/>
+    </div>
+  )
+}
+
+export default App
+
